96e7aa608df0c039ebaf4367d32d2714a4e71f71
6a6ak <babak.ahari@gmail.com>
2025-06-04 23:23:53 +0200

    fix: simplify profile header box by removing redundant class for cleaner layout

--- index.php
+++ index.php
@@ -253,7 +253,7 @@ $username = isset($_GET['username']) ? htmlspecialchars($_GET['username']) : '';
 
 
 <?php if ($username): ?>
-<div class="card profile-header-box">
+<div class="card">
     <div class="profile-pic-square">
         <img src="https://github.com/<?= $username ?>.png" alt="GitHub Avatar">
     </div>

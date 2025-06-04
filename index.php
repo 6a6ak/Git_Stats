bf23f93532cb0f6cfcd17fdf96057918bd79ee82
6a6ak <babak.ahari@gmail.com>
2025-06-04 23:20:23 +0200

    fix: update stats heading to use card styling for improved layout consistency

--- index.php
+++ index.php
@@ -274,7 +274,7 @@ $username = isset($_GET['username']) ? htmlspecialchars($_GET['username']) : '';
 <?php endif; ?>
 
 <?php if ($username): ?>
-    <div class="stats-heading">
+    <div class="card"> /* stats-heading */
         Stats for <strong><?= $username ?></strong>
     </div>
     <div class="stats-container">
